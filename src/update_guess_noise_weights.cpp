80ae079282e3f5273111f1153b6c2d751adba26a
a1arakkal <alan-arakkal@uiowa.edu>
2025-06-03 11:19:56 -0600

    Create update_guess_noise_weights.cpp

---
+++ src/update_guess_noise_weights.cpp
@@ -0,0 +1,35 @@
+
+#include <RcppArmadillo.h>
+
+// [[Rcpp::depends(RcppArmadillo)]]
+
+
+// [[Rcpp::export]]
+void update_guess_noise_weights(arma::colvec& guess_noise_weights, arma::mat prob_matrix_W, double precision_noise_weights, double prior_mean_noise, double prior_var_noise, Rcpp::String family){
+
+  int M = prob_matrix_W.n_rows;
+ 
+  
+  for(int m = 0; m < M; m++){
+
+    if (family == "poisson"){
+    
+   
+    } else if (family == "exp_lognormal"){
+
+      double p1 = ()
+
+ 
+    } else {
+      
+      double p1 = (precision_noise_weights * arma::sum(prob_matrix_W.col(4) % arma::log(prob_matrix_W.col(2)))) + ((1.0/prior_var_noise) * prior_mean_noise);
+      double p2 = (precision_noise_weights * arma::sum(prob_matrix_W.col(4))) + (1.0/prior_var_noise));
+      guess_noise_weights(0) = p1/p2;
+   
+    }
+
+  }
+
+      
+}
+
