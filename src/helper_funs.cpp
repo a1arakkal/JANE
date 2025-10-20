f0efe3355f289287256ecfb932697e71c9af78cb
a1arakkal <alan-arakkal@uiowa.edu>
2025-10-20 14:11:05 -0600

    is_sympd id scaled by F norm of matrix so can be large value set to 100 * datum::eps instead

--- src/helper_funs.cpp
+++ src/helper_funs.cpp
@@ -62,7 +62,7 @@ double lognormal_density(double w, double precision, double mean, double log){
 // [[Rcpp::export]]
 arma::colvec solve_only_sympd(arma::mat A, arma::colvec b){
 
-    if (!A.is_sympd()) {
+    if (!A.is_sympd(100 * datum::eps)) {
       Rcpp::stop("Matrix is not symmetric positive definite.");
     }
 
